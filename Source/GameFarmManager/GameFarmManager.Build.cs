8ad0793805c0e624d434bb6f10b66c36b18a58d0
DooMShortS <duplessisruan.hawk@gmail.com>
2026-07-07 16:46:28 +0200

    Initial commit: UE 5.7 FP template + GameFarm content structure

---
+++ Source/GameFarmManager/GameFarmManager.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GameFarmManager : ModuleRules
+{
+	public GameFarmManager(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"GameFarmManager",
+			"GameFarmManager/Variant_Horror",
+			"GameFarmManager/Variant_Horror/UI",
+			"GameFarmManager/Variant_Shooter",
+			"GameFarmManager/Variant_Shooter/AI",
+			"GameFarmManager/Variant_Shooter/UI",
+			"GameFarmManager/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
